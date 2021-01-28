8de1ccd4dc1acb12d31813db7599942bdfb79580
Gregory Schultz <schultzie@users.noreply.github.com>
2021-01-28 12:05:57 -0800

    Create storage.js
    Just started with static json delivery right now. Messy but can be improved down the line.

---
+++ src/storage.js
@@ -0,0 +1,225 @@
+export var kb_storage = new Object();
+kb_storage.vpn = [
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": ":wave: Please give this a look: This document may help."
+        }
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "*Type:*\nInternal Knowledge Base Article :open_book:\n*Location:*\n<https://www.google.com>"
+        },
+        "accessory": {
+            "type": "image",
+            "image_url": "https://en.wikipedia.org/wiki/Open_Source_Initiative#/media/File:Opensource.svg",
+            "alt_text": "computer thumbnail"
+        }
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "After reading the article please select if you still need help."
+        },
+        "accessory": {
+            "type": "static_select",
+            "placeholder": {
+                "type": "plain_text",
+                "text": "Solved / Not Solved",
+                "emoji": true
+            },
+            "options": [
+                {
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Solved  :thumbsup:",
+                        "emoji": true
+                    },
+                    "value": "value-0"
+                },
+                {
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Not Solved  :thumbsdown:",
+                        "emoji": true
+                    },
+                    "value": "value-1"
+                }
+            ],
+            "action_id": "static_select-action"
+        }
+    }
+]
+
+kb_storage.password = [
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": ":wave: Please give this a look: This document may help."
+        }
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "*Type:*\nInternal Knowledge Base Article :open_book:\n*Location:*\n<https://www.google.com>"
+        },
+        "accessory": {
+            "type": "image",
+            "image_url": "https://en.wikipedia.org/wiki/Open_Source_Initiative#/media/File:Opensource.svg",
+            "alt_text": "computer thumbnail"
+        }
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "After reading the article please select if you still need help."
+        },
+        "accessory": {
+            "type": "static_select",
+            "placeholder": {
+                "type": "plain_text",
+                "text": "Solved / Not Solved",
+                "emoji": true
+            },
+            "options": [
+                {
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Solved  :thumbsup:",
+                        "emoji": true
+                    },
+                    "value": "value-0"
+                },
+                {
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Not Solved  :thumbsdown:",
+                        "emoji": true
+                    },
+                    "value": "value-1"
+                }
+            ],
+            "action_id": "static_select-action"
+        }
+    }
+]
+
+kb_storage.network = [
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": ":wave: Please give this a look: This document may help."
+        }
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "*Type:*\nInternal Knowledge Base Article :open_book:\n*Location:*\n<https://www.google.com>"
+        },
+        "accessory": {
+            "type": "image",
+            "image_url": "https://en.wikipedia.org/wiki/Open_Source_Initiative#/media/File:Opensource.svg",
+            "alt_text": "computer thumbnail"
+        }
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "After reading the article please select if you still need help."
+        },
+        "accessory": {
+            "type": "static_select",
+            "placeholder": {
+                "type": "plain_text",
+                "text": "Solved / Not Solved",
+                "emoji": true
+            },
+            "options": [
+                {
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Solved  :thumbsup:",
+                        "emoji": true
+                    },
+                    "value": "value-0"
+                },
+                {
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Not Solved  :thumbsdown:",
+                        "emoji": true
+                    },
+                    "value": "value-1"
+                }
+            ],
+            "action_id": "static_select-action"
+        }
+    }
+]
+
+kb_storage.mac = [
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": ":wave: Please give this a look: This document may help."
+        }
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "*Type:*\nInternal Knowledge Base Article :open_book:\n*Location:*\n<https://www.google.com>"
+        },
+        "accessory": {
+            "type": "image",
+            "image_url": "https://en.wikipedia.org/wiki/Open_Source_Initiative#/media/File:Opensource.svg",
+            "alt_text": "computer thumbnail"
+        }
+    },
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "After reading the article please select if you still need help."
+        },
+        "accessory": {
+            "type": "static_select",
+            "placeholder": {
+                "type": "plain_text",
+                "text": "Solved / Not Solved",
+                "emoji": true
+            },
+            "options": [
+                {
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Solved  :thumbsup:",
+                        "emoji": true
+                    },
+                    "value": "value-0"
+                },
+                {
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Not Solved  :thumbsdown:",
+                        "emoji": true
+                    },
+                    "value": "value-1"
+                }
+            ],
+            "action_id": "static_select-action"
+        }
+    }
+]
+
