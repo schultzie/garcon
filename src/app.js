fbcbb80e56c7132299626d9bdce75f20299f1b4b
Gregory Schultz <schultzie@users.noreply.github.com>
2021-01-28 11:57:14 -0800

    update app.js
    bot listens to messages in a slack channel, matches keywords with a knowledgebase article in storage.js, and pushed it back as a threaded reply.

--- src/app.js
+++ src/app.js
@@ -1,35 +1,58 @@
 import { RTMClient }  from '@slack/rtm-api'
-import { SLACK_OAUTH_TOKEN, BOT_SPAM_CHANNEL } from './constants'
-import  { WebClient } from '@slack/web-api';
-const packageJson = require('../package.json')
+import { WebClient } from '@slack/web-api'
+import { SLACK_OAUTH_TOKEN } from './coins'
+import {kb_storage} from './storage'
 
 const rtm = new RTMClient(SLACK_OAUTH_TOKEN);
 const web = new WebClient(SLACK_OAUTH_TOKEN);
 
-rtm.start()
-  .catch(console.error);
+let randomText;
+let the_chan;
+let the_user;
+let the_ts;
+
+
+function the_payload(randomText, the_chan, the_user, the_ts){
+    if (randomText.includes('mac' && 'network')){
+        return send_kb(the_chan, the_user, kb_storage['mac'], the_ts);
+    }
+    if (randomText.includes('vpn')){
+        return send_kb(the_chan, the_user, kb_storage['vpn'], the_ts);
+    }
+    if (randomText.includes('password')){
+        return send_kb(the_chan, the_user, kb_storage['password'], the_ts);
+    }
+    if (randomText.includes('network')){
+        return send_kb(the_chan, the_user, kb_storage['network'], the_ts);
+    }
+};
+
+//sends the reply to the channel
+function send_kb(channelId, userId, storageId, theTs) {
+    web.chat.postMessage({
+        "channel": channelId,
+        "username": 'Garcon',
+        "thread_ts": theTs,
+        "text": 'Garcon is in the channel.',
+        "blocks": storageId,
+        "Null": userId
+    })
+};
+
+rtm.start().catch(console.error);
 
 rtm.on('ready', async () => {
     console.log('bot started')
-    sendMessage(BOT_SPAM_CHANNEL, `Bot version ${packageJson.version} is online.`)
 })
 
+//checks slack message and gathers data and passes it to the_payload function 
 rtm.on('slack_event', async (eventType, event) => {
-    if (event && event.type === 'message'){
-        if (event.text === '!hello') {
-            hello(event.channel, event.user)
-        }
+    if (event && eventType === 'message'){
+        randomText = event.text
+        the_chan = event.channel
+        the_user = event.user
+        the_ts = event.ts
+        if (typeof randomText === 'undefined') return;
+        the_payload(randomText, the_chan, the_user, the_ts);
     }
 })
-
-
-function hello (channelId, userId) {
-    sendMessage(channelId, `Heya! <@${userId}>`)
-}
-
-async function sendMessage(channel, message) {
-    await web.chat.postMessage({
-        channel: channel,
-        text: message,
-    })
-}
